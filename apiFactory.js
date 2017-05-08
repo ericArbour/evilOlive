2b8ccd7ef3e77348862c54d7903887ae0aecbe2b
Brian Barnes <barnesb7@gmail.com>
2017-05-08 14:18:49 -0400

    testing 2nd controller

--- apiFactory.js
+++ apiFactory.js
@@ -3,11 +3,11 @@ var app = angular.module('myModule');
 app.factory('searchFactory', ['$http',function($http){
 
   var obj = {};
-  var recipeArray = [];
+  var recipeHolder;
   // console.log('factory works');
   obj.setSearch = function(userInput){
-    console.log(userInput);
-    $http({
+    // console.log(userInput);
+    recipeHolder = $http({
       method: 'GET',
       url: 'https://community-food2fork.p.mashape.com/search?key=b5e7a2265c20832a3e572ad6ce2c5ebc&q=' + userInput,
       headers: {
@@ -15,11 +15,14 @@ app.factory('searchFactory', ['$http',function($http){
         'Accept': 'application/json'
       }
     }).then(function(response) {
-      recipeArray = response.data.recipes;
-      console.log(response);
+      return response.data.recipes;
+      // console.log(response);
     }, function(error) {
-      console.log(error);
+        return error;
     });
+  }
+  obj.getSearch = function(){
+    return recipeHolder;
   }
   return obj;
 }]);
