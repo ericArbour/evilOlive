141f70225f0593d9863c0715826c6afacb578145
Brian Barnes <barnesb7@gmail.com>
2017-05-08 12:58:09 -0400

    factory works

---
+++ apiFactory.js
@@ -0,0 +1,23 @@
+var app = angular.module('myModule');
+
+app.factory('searchFactory', ['$http',function($http){
+
+  var obj = {};
+  var recipeArray = [];
+  console.log('factory works');
+  obj.call = function(){
+    $http({
+      method: 'GET',
+      url: 'https://community-food2fork.p.mashape.com/search?key=b5e7a2265c20832a3e572ad6ce2c5ebc&q=shredded+chicken%2C+mushrooms',
+      headers: {
+        'X-Mashape-Key': '0ngYLs5AJmmshkhm9tUAQtiVyuzpp13kvEKjsnzMjozH8wa6rv',
+        'Accept': 'application/json'
+      }
+    }).then(function(response) {
+      console.log(response)
+    }, function(error) {
+      console.log(error)
+    });
+  }
+  return obj;
+}]);
--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,19 +1,21 @@
 var app = angular.module('myModule');
 
-app.controller('apiController', ['$scope', '$http', function($scope, $http) {
+app.controller('apiController', ['$scope', '$http', 'searchFactory', function($scope, $http, searchFactory) {
   alert('hi');
-  $http({
-    method: 'GET',
-    url: 'https://community-food2fork.p.mashape.com/search?key=b5e7a2265c20832a3e572ad6ce2c5ebc&q=shredded+chicken%2C+mushrooms',
-    headers: {
-      'X-Mashape-Key': '0ngYLs5AJmmshkhm9tUAQtiVyuzpp13kvEKjsnzMjozH8wa6rv',
-      'Accept': 'application/json'
-    }
-  }).then(function(response) {
-    console.log(response)
-  }, function(error) {
-    console.log(error)
-  });
+
+  searchFactory.call();
+  // $http({
+  //   method: 'GET',
+  //   url: 'https://community-food2fork.p.mashape.com/search?key=b5e7a2265c20832a3e572ad6ce2c5ebc&q=shredded+chicken%2C+mushrooms',
+  //   headers: {
+  //     'X-Mashape-Key': '0ngYLs5AJmmshkhm9tUAQtiVyuzpp13kvEKjsnzMjozH8wa6rv',
+  //     'Accept': 'application/json'
+  //   }
+  // }).then(function(response) {
+  //   console.log(response)
+  // }, function(error) {
+  //   console.log(error)
+  // });
   // $http.get('http://food2fork.com/api/search?key={b5e7a2265c20832a3e572ad6ce2c5ebc}&q=shredded%20chicken').then(function(response) {
   //   console.log(response);
   // }, function(error) {
